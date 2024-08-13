580df6d2f97fe6650ef03e7d70a4918cdbdbdb82
DiegoJuarezV <diegojve27@gmail.com>
2024-08-12 19:55:41 -0600

    Creando nuevo proyecto de React para el parcial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+        <Card name="Diego"/>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = ( {name} ) => {
+  
+  return (
+    <>
+      <h3>Hola, soy una tarjeta</h3>
+      <p>Mi nombre es {name}</p>
+    </>
+  )
+}
+
+export default Card
